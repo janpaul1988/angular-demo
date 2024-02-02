ee934fac0ff2a5fc9c2830ec3d94ed6663a16461
janpaul1988 <janpaulbrasser88@gmail.com>
2024-02-02 23:46:41 +0100

    Remove service layer

--- spring-boot-app/src/main/java/org/example/angulardemo/controller/ProductController.java
+++ spring-boot-app/src/main/java/org/example/angulardemo/controller/ProductController.java
@@ -2,28 +2,23 @@ package org.example.angulardemo.controller;
 
 import org.example.angulardemo.entity.Product;
 import org.example.angulardemo.repo.ProductRepository;
-import org.example.angulardemo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost")
 @RequestMapping("/products")
 public class ProductController {
-    private final ProductService productService;
     private final ProductRepository productRepository;
 
     @Autowired
-    public ProductController(ProductService productService, ProductRepository productRepository) {
-        this.productService = productService;
+    public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @GetMapping
-    public List<Product> getAllProducts() {
-       return productService.getAllProducts();
+    public Iterable<Product> getAllProducts() {
+       return productRepository.findAll();
     }
 
     @PostMapping
