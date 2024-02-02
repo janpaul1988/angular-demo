07a82cd136a81e1879aa7d371c05cffc10e243f6
janpaul1988 <janpaulbrasser88@gmail.com>
2024-02-02 23:47:19 +0100

    Add JUnit tests

--- spring-boot-app/src/main/java/org/example/angulardemo/controller/ProductController.java
+++ spring-boot-app/src/main/java/org/example/angulardemo/controller/ProductController.java
@@ -1,8 +1,12 @@
 package org.example.angulardemo.controller;
 
+import jakarta.validation.Valid;
 import org.example.angulardemo.entity.Product;
 import org.example.angulardemo.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,24 +26,40 @@ public class ProductController {
     }
 
     @PostMapping
-    public Product addProduct(@RequestBody Product newProduct) {
-        return productRepository.save(newProduct);
+    public ResponseEntity<?> addProduct(@Valid @RequestBody Product newProduct, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+
+        if (newProduct.getId() != null) {
+            return new ResponseEntity<>("Id field should not be set", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(productRepository.save(newProduct), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id) {
-        productRepository.deleteById(id);
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    productRepository.delete(product);
+                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                })
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/{id}")
-    public void updateProduct(@PathVariable Long id, @RequestBody Product productUpdate) {
-
-        productRepository.findById(id)
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @Valid @RequestBody Product productUpdate, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+        return productRepository.findById(id)
                 .map(product -> {
                     product.setExtId(productUpdate.getExtId());
                     product.setName(productUpdate.getName());
                     product.setDescription(productUpdate.getDescription());
-                    return productRepository.save(product);
-                });
+                    return new ResponseEntity<>(productRepository.save(product), HttpStatus.OK);
+                })
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
--- spring-boot-app/src/main/java/org/example/angulardemo/entity/Product.java
+++ spring-boot-app/src/main/java/org/example/angulardemo/entity/Product.java
@@ -3,42 +3,28 @@ package org.example.angulardemo.entity;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
 @Entity
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
-
+    @NotEmpty
+    @Column(nullable = false)
     private String extId;
+    @NotEmpty
+    @Column(nullable = false)
     private String name;
     private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getExtId() {
-        return extId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setExtId(String extId) {
-        this.extId = extId;
-    }
 }
